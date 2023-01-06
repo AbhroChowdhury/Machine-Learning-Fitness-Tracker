2e1b6a709d069b771840c09aab46c2f818b84c0f
AbhroChowdhury48HD <achowdhury@48hourdiscovery.com>
2023-01-06 10:51:58 -0700

    updates on webcam functionality

--- webcam.py
+++ webcam.py
@@ -2,11 +2,18 @@
 File responsible for webcam feed
 '''
 
-import cv2 as cv
+import cv2
 
 # accessing webcam feed with opencv library
 # cv.VideoCapture allows us to read webcam video as seperate image frames
 class Webcam:
     def __init__(self):
-        self.webcam = cv.VideoCapture(0) # 0  indicates that default camera should be used
-        
+        self.webcam = cv2.VideoCapture(0)     # 0 indicates that default webcam should be used
+        if not self.webcam.isOpened():
+            raise ValueError("No webcam has been detected")     # use isOpened returns False, then webcam is not detected
+        self.width = (cv2.CAP_PROP_FRAME_WIDTH)     # get height and width
+        self.height = (cv2.CAP_PROP_FRAME_HEIGHT)
+
+
+    def individual_frames(self):
+        pass
