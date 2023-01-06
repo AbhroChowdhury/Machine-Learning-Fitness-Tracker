da815fd048d9dfe992cc19e37ca4ce6708ea2f1f
AbhroChowdhury48HD <achowdhury@48hourdiscovery.com>
2023-01-06 10:44:34 -0700

    setting up videocapture

--- webcam.py
+++ webcam.py
@@ -4,3 +4,9 @@ File responsible for webcam feed
 
 import cv2 as cv
 
+# accessing webcam feed with opencv library
+# cv.VideoCapture allows us to read webcam video as seperate image frames
+class Webcam:
+    def __init__(self):
+        self.webcam = cv.VideoCapture(0) # 0  indicates that default camera should be used
+        
