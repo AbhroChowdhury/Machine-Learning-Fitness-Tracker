11ff61968afde3a8a946c7c202d827201d3d8ac2
AbhroChowdhury48HD <achowdhury@48hourdiscovery.com>
2023-01-06 23:30:18 -0700

    created function to handle individual frames

--- webcam.py
+++ webcam.py
@@ -16,6 +16,15 @@ class Webcam:
 
 
     def individual_frames(self):
-        pass
+        if self.webcam.isOpened():
+            success, frame = self.webcam.read()     # check if frame is read successfully, and also return frame itself
+            if success:
+                return (success, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))     # by default opencv is in bgr and so convert frame to rgb
+            else:
+                return success, None
+        else:
+            return None
 
-#
+    
+
+
