6b3327916a978433ab6a9150c9b4d7e5d6288110
AbhroChowdhury <abhrajyo@ualberta.ca>
2023-01-13 18:57:17 -0700

    gui updates

--- gui.py
+++ gui.py
@@ -32,13 +32,35 @@ class GUI:
         self.btn_toggle_counter = ctk.CTkButton(self.window, text='Toggle Counting', command='self.toggle_counting')
         self.btn_toggle_counter.pack(anchor=tk.CENTER, expand=True)
 
-        self.btn_class_one = ctk.CTkButton(self.window, text='Toggle Counting', command='self.toggle_counting')
-        self.btn_toggle_counter.pack(anchor=tk.CENTER, expand=True)
+        self.model = None
+
+        self.btn_class_one = ctk.CTkButton(self.window, text='Extended', width=50, command=lambda: self.save_for_class(1))
+        self.btn_class_one.pack(anchor=tk.CENTER, expand=True)
+
+        self.btn_class_one = ctk.CTkButton(self.window, text='Contracted',width=50, command=lambda: self.save_for_class(2))
+        self.btn_class_two.pack(anchor=tk.CENTER, expand=True)
+
+        self.btn_train = ctk.CTkButton(self.window, text='Train Model',width=50, command=lambda: self.model.train_model(self.counters))
+        self.btn_train.pack(anchor=tk.CENTER, expand=True)
+
+        self.btn_reset = ctk.CTkButton(self.window, text='Reset',width=50, command=self.reset)
+        self.btn_reset.pack(anchor=tk.CENTER, expand=True)
+
+        self.main_label = ctk.CTkLabel(self.window, text=f"{self.rep_counter}")
+        self.main_label.config(font=('Arial', 24))
+        self.main_label.pack(anchor=tk.CENTER, expand=True)
 
     def update(self):
         pass
 
     def toggle_counting(self):
+        
+
+    def save_for_class(self, class_num):
         pass
+
+    def reset(self):
+        pass
+
 
 
