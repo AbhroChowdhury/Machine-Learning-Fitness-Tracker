96bbf05431989630bfe05a92b363062ec4e9fe16
AbhroChowdhury <abhrajyo@ualberta.ca>
2023-01-13 20:03:44 -0700

    fixing errors

--- gui.py
+++ gui.py
@@ -21,7 +21,7 @@ class GUI:
         self.contracted = False
         self.previous = 0
         self.enablemodel = False
-        self.camera = webcam.Webcam()
+        self.webcam = webcam.Webcam()
 
         self.delay = 15
         self.window.attributes("-topmost", True)
@@ -29,26 +29,26 @@ class GUI:
         self.update()
 
     def initialize_gui(self):
-        self.canvas = tk.TkCanvas(self.window, width=self.webcam.width, height=self.webcam.height)
+        self.canvas = tk.Canvas(self.window, width=self.webcam.width, height=self.webcam.height)
         self.canvas.pack()
-        self.btn_toggle_counter = tk.TkButton(self.window, text='Toggle Counting', command='self.toggle_counting')
+        self.btn_toggle_counter = tk.Button(self.window, text='Toggle Counting', command='self.toggle_counting')
         self.btn_toggle_counter.pack(anchor=tk.CENTER, expand=True)
 
         self.model = trainingmodel.Model()
 
-        self.btn_class_one = tk.TkButton(self.window, text='Extended', width=50, command=lambda: self.save_for_class(1))
+        self.btn_class_one = tk.Button(self.window, text='Extended', width=50, command=lambda: self.save_for_class(1))
         self.btn_class_one.pack(anchor=tk.CENTER, expand=True)
 
-        self.btn_class_one = tk.TkButton(self.window, text='Contracted',width=50, command=lambda: self.save_for_class(2))
+        self.btn_class_one = tk.Button(self.window, text='Contracted',width=50, command=lambda: self.save_for_class(2))
         self.btn_class_two.pack(anchor=tk.CENTER, expand=True)
 
-        self.btn_train = tk.TkButton(self.window, text='Train Model',width=50, command=lambda: self.model.train_model(self.counters))
+        self.btn_train = tk.Button(self.window, text='Train Model',width=50, command=lambda: self.model.train_model(self.counters))
         self.btn_train.pack(anchor=tk.CENTER, expand=True)
 
-        self.btn_reset = tk.TkButton(self.window, text='Reset',width=50, command=self.reset)
+        self.btn_reset = tk.Button(self.window, text='Reset',width=50, command=self.reset)
         self.btn_reset.pack(anchor=tk.CENTER, expand=True)
 
-        self.main_label = tk.TkLabel(self.window, text=f"{self.rep_counter}")
+        self.main_label = tk.Label(self.window, text=f"{self.rep_counter}")
         self.main_label.config(font=('Arial', 24))
         self.main_label.pack(anchor=tk.CENTER, expand=True)
 
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 ''''
 File responsible for running the program
 '''
+
+import gui
+
+def main():
+    gui.GUI()
+
+if __name__ == '__main__':
+    main()
