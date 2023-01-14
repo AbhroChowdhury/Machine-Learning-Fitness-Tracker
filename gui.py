0ad94cf270c5dc0abf1b68edef16ba275f72c601
AbhroChowdhury <abhrajyo@ualberta.ca>
2023-01-13 18:30:04 -0700

    starting gui

--- gui.py
+++ gui.py
@@ -1,3 +1,12 @@
 '''
 This file is responsible for the graphical user interface. This will be done with TKinter
 '''
+
+import tkinter as tk
+import customtkinter as ctk
+import cv2
+import numpy as np
+import os 
+import PIL.Image, PIL.ImageTk
+import webcam
+
