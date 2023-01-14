91c13f66ef345e14b687f00c4f24752b42343acd
AbhroChowdhury <abhrajyo@ualberta.ca>
2023-01-13 19:37:01 -0700

    updates to model

--- trainingmodel.py
+++ trainingmodel.py
@@ -18,4 +18,11 @@ class Model:
             img = cv2.imread(f"1/frame{i}.jpg")[:,:,0]
             img = img.reshape(16950)
             img_list = np.append(img_list, [img])
-            class_list = np.append(class_list, 1)
+            class_list = np.append(class_list, 2)
+
+        img_list = img_list.reshape(counters[0]-1+counters[1]-1, 16950)
+        self.model.fit(img_list, class_list)
+        print("Training Model Completed")
+
+    def predict(self):
+        
